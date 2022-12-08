9637fff6f8e62a9186c62307fe28cf84a0021f22
Dennis.K.Y.Chou <dennis.ky.chou@maxnerva.com>
2022-12-08 17:21:46 +0800

    Update Service Mesh demo files

---
+++ 04_ServiceMesh/01_bluegreen_deploy/scripts/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+URL=$(oc get route -n istio-system -l maistra.io/gateway-name=bookinfo-gateway|awk 'NR > 1 {print $2}')
+echo "Call URL: $URL"
+echo "---------------"
+curl -I "http://$URL/productpage"
