23c4be2be499aeca73d36e1c73bc931d295f4cb8
molasun <mola79321@gmail.com>
2022-12-19 22:37:07 +0800

    fix serverless load test

--- 06_Serverless/01_autoscale/scripts/load-test.sh
+++ 06_Serverless/01_autoscale/scripts/load-test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-URL=$(oc get services.serving.knative.dev autoscale-go |awk 'NR > 1 {print $2}')
+URL=$(oc get services.serving.knative.dev autoscale-go -n serverless-demo |awk 'NR > 1 {print $2}')
 echo "Call URL: $URL"
 echo "---------------"
 
 # 50 concurrency for 30 seconds
 hey -z 30s -c 50 \
   "$URL?sleep=100&prime=10000&bloat=5" \
-  && kubectl get pods
+  && kubectl get pods -n serverless-demo
