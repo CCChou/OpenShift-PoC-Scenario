4154126c2d8bea82b52fa28b1e06730a0ea0ffcb
dennischou <dses5900@gmail.com>
2023-01-18 08:33:57 +0800

    Update Service Mesh Scenario 1. Add delete scripts for clean up 2. Modify istio components yaml files for clean up

---
+++ 04_ServiceMesh/00_istio_setup/scripts/delete_app.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    export NAMESPACE="bookinfo"
+fi
+if [ "$#" -eq 1 ]; then
+    export NAMESPACE=$1
+fi 
+
+
+envsubst < yaml/bookinfo.yaml | oc delete -f -
