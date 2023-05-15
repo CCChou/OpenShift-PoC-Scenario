71fcf0647271cad53ae1d980691149bbb69e09d2
molasun <mola79321@gmail.com>
2023-05-15 16:51:53 +0800

    Update cleanup.sh in 05_ClusterManagement/03_policyorder

--- 05_ClusterManagement/03_policyorder/script/cleanup.sh
+++ 05_ClusterManagement/03_policyorder/script/cleanup.sh
@@ -7,5 +7,19 @@ do
   oc delete policy $i;
 done
 
+# 刪除 placementbinding
+for i in $(oc get placementbinding -o wide -n default | sed -n '2,$p' | cut -d ' ' -f1)
+do 
+  echo "=== delete placementbinding " $i " ===" ;
+  oc delete placementbinding $i;
+done
+
+# 刪除 placementrule
+for i in $(oc get placementrule -o wide -n default | sed -n '2,$p' | cut -d ' ' -f1)
+do 
+  echo "=== delete placementrule " $i " ===" ;
+  oc delete placementrule $i;
+done
+
 # 刪除 namespace
 oc delete projects minio-dev openshift-operators-redhat openshift-logging --force
