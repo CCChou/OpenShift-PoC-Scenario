75b4f3a19bf95cab3c8b3f3fea280b78158acf58
dennischou <dses5900@gmail.com>
2023-12-13 09:32:20 +0800

    Fix bugs 1. Update storage class name for new platform 2. Fix typo in scripts

--- 03_Operation/04_logging_loki/script/setup.sh
+++ 03_Operation/04_logging_loki/script/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 oc apply -f yaml/loki-stack.yaml
-oc apply -f yaml/clusterlogging.yaml
+oc apply -f yaml/clusterlogging-loki.yaml 
 oc process -f yaml/eventrouter.yaml | oc apply -n openshift-logging -f -
 oc apply -f yaml/auditlogforward.yaml
